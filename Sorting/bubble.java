4b5357740c517db94b146218a56d0b040f38c137
Akash Rai <127041159+akazza07@users.noreply.github.com>
2024-10-15 19:27:24 +0530

    Add files via upload

---
+++ Sorting/bubble.java
@@ -0,0 +1,32 @@
+package Sorting;
+
+public class bubble {
+    public static void bubbleSort(int arr[]){
+
+        for(int i = 0 ; i < arr.length - 1 ; i++){
+             for( int j = 0 ; j < arr.length - 1 - i ; j++){
+                if( arr[j] > arr[j+1] ){
+                 
+                    //Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];;
+                    arr[j+1] = temp ; 
+                }
+             }
+        }
+    }
+    // print 
+
+    public static void printarr(int arr[]){
+
+        for( int i = 0 ; i < arr.length ; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[]args){
+        int arr[] = {5,4,1,3,2};
+        bubbleSort(arr);
+        printarr(arr);
+    }
+}
